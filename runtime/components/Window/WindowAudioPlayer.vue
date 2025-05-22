119ef975a0650ed65ce7742c167c47ba732e6419
dxlliv <hello@dxlliv.dev>
2025-05-22 17:42:48 +0200

    refactor: Support multiple mimeType

--- runtime/components/Window/WindowAudioPlayer.vue
+++ runtime/components/Window/WindowAudioPlayer.vue
@@ -10,21 +10,42 @@ const { t } = useI18n()
 
 const filePath = loadAudioUrl(props.window.meta.path)
 
-function loadAudioUrl(path: string) {
-  if (!path) {
+function loadAudioUrl(pathString: string) {
+  if (!pathString) {
     return
   }
 
-  if (path.startsWith('http')) {
-    return path
+  if (pathString.startsWith('http')) {
+    return pathString
   }
 
   try {
-    const buffer = fs.readFileSync(path)
-    const blob = new Blob([buffer], { type: 'audio/mp3' })
+    const buffer = fs.readFileSync(pathString)
+    const lastDotIndex = pathString.lastIndexOf('.')
+    const lastSlashIndex = pathString.lastIndexOf('/')
+    let ext = ''
+    if (lastDotIndex > lastSlashIndex) {
+      ext = pathString.substring(lastDotIndex).toLowerCase()
+    }
+
+    let mimeType
+
+    switch (ext) {
+      case '.mp3':
+        mimeType = 'audio/mp3'
+        break
+      case '.wav':
+        mimeType = 'audio/wav'
+        break
+      default:
+        console.warn(`File is not supported: ${ext}`)
+        return
+    }
+
+    const blob = new Blob([buffer], { type: mimeType })
     return URL.createObjectURL(blob)
   } catch (err) {
-    console.error("Error while loading the audio", err)
+    console.error('Error while loading the audio', err)
     return
   }
 }
